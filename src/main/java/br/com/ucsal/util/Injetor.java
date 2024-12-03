c320998a8afa76401ec2ca205eb2d5daed318b87
Murilo Sena <132272353+0mss@users.noreply.github.com>
2024-12-03 19:00:26 -0300

    Update Injetor.java

--- src/main/java/br/com/ucsal/util/Injetor.java
+++ src/main/java/br/com/ucsal/util/Injetor.java
@@ -8,12 +8,10 @@ import br.com.ucsal.persistencia.ProdutoRepository;
 import br.com.ucsal.service.ProdutoService;
 
 
-/*
-  Classe utilitária responsável por injetar dependências em objetos cujos campos estão 
+/* Classe utilitária responsável por injetar dependências em objetos cujos campos estão 
   anotados com @Inject. Utiliza reflexão para localizar os campos e resolve as dependências 
   com base no tipo esperado. Atualmente, suporta a injeção de ProdutoService, 
-  que é criado com um ProdutoRepository obtido via PersistenciaFactory.
- */
+  que é criado com um ProdutoRepository obtido via PersistenciaFactory. */
 public class Injetor {
 
     public static void injetarDependencias(Object objeto) {
